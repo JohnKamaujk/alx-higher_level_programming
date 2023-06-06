a17162e704315ce87ae29f9277740f89cec5e9cd
JohnKamaujk <johnjkkamau@gmail.com>
2023-06-06 12:37:10 +0300

    :sparkles:chore:print comb2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - John Mwega
+# Author - Johnnyjk
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
