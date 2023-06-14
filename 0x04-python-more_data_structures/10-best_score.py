84c74a60bd8322f55df786c03c04fa251133fe6f
JohnKamaujk <johnjkkamau@gmail.com>
2023-06-14 20:20:16 +0300

    :sparkles:chore:solve challenge

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,11 +4,12 @@ def best_score(a_dictionary):
     """
     A function that returns a key with the biggest integer value.
     """
-    max_key = None
-    max_value = 0
+    if a_dictionary:
+        max_key = ""
+        max_value = 0
 
-    for key, value in a_dictionary.items():
-        if value > max_value:
-            max_key = key
-            max_value = value
+        for key, value in a_dictionary.items():
+            if value > max_value:
+                max_key = key
+                max_value = value
     return max_key
