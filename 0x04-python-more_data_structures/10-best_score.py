9b7061e35c4a2facc08ab6447f5d8ea47d61aed2
JohnKamaujk <johnjkkamau@gmail.com>
2023-06-14 20:30:11 +0300

    :sparkles:chore:solve code challenge

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -9,7 +9,7 @@ def best_score(a_dictionary):
         max_value = 0
 
         for key, value in a_dictionary.items():
-            if a_dictionary[key] > max_value:
+            if value> max_value:
                 max_key = key
-                max_value = a_dictionary[key]
+                max_value = value
         return max_key
