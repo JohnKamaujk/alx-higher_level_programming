6afe38f185eee95d75f108c31f272929d066668c
JohnKamaujk <johnjkkamau@gmail.com>
2023-07-13 14:52:57 +0300

    :sparkles:chore:implement rectangle model features

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@
 from models.base import Base
 
 
-class rectangle(Base):
+class Rectangle(Base):
     """
         class Rectangle inherits from Base.
         Methods:
@@ -14,13 +14,13 @@ class rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-            Initializes the instance of the class..
+            Initializes the instance of the class.
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -81,8 +81,8 @@ class rectangle(Base):
         """
         if type(value) != int:
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be > 0")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -102,6 +102,23 @@ class rectangle(Base):
         """
         if type(value) != int:
             raise TypeError("y must be an integer")
-        if value <= 0:
-            raise ValueError("y must be > 0")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+            returns the area of the Rectangle instance.
+        """
+        return (self.__height * self.__width)
+
+    def display(self):
+        """
+            prints to stdout the Rectangle instance with '#'
+        """
+        rectangle = ""
+        symbol = "#"
+
+        for row in range(self.__height):
+            rectangle += (symbol*self.__width) + "\n"
+        print(rectangle, end="")
