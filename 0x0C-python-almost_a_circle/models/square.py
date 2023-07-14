5d50199be687f26e211d323a252635bca0156615
JohnKamaujk <johnjkkamau@gmail.com>
2023-07-14 13:40:29 +0300

    :sparkles:chore:square model implementation

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+"""Defines square model class which inherits from rectangle."""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+        class Square inherits from Rectangle.
+        Methods:
+            __init__()
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+            initialises Square (overrides Rectangle init)
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+            returns the size of the square
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+            sets the value of size
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+            assigns key/value argument to attributes
+            kwargs skipped if args == empty
+            Args:
+                *args -  variable number of no-keyword args
+                **kwargs - variable number of keyworded args
+        """
+        if len(args) == 0:
+            for key, val in kwargs.items():
+                self.__setattr__(key, val)
+            return
+
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
+
+    def __str__(self):
+        """
+            returns a string format overides rectangle __str__ method
+        """
+        return "[{}] ({}) {}/{} - {}".format(type(self).__name__, self.id,
+                                             self.x, self.y,
+                                             self.width)
