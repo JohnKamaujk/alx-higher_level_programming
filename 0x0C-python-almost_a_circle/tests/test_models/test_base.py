d9015cc59afc7b7ec1b15e3f3f5bf21014da6e28
JohnKamaujk <johnjkkamau@gmail.com>
2023-07-16 11:50:03 +0300

    :sparkles:feat:Tests initialization

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# test_base.py
+"""Defines unittests for base.py.
+Unittest classes:
+    TestBase_instantiation - line 23
+"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
