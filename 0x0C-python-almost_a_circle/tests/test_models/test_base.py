fa5608144e1c2f4d1e41ac821cbedb0910e8566d
JohnKamaujk <johnjkkamau@gmail.com>
2023-07-17 12:26:23 +0300

    :sparkles:Base class tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,14 +2,14 @@
 # test_base.py
 """Defines unittests for base.py.
 Unittest classes:
-    TestBase_instantiation - line 23
-    TestBase_to_json_string - line 110
-    TestBase_save_to_file - line 156
-    TestBase_from_json_string - line 234
-    TestBase_create - line 288
-    TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
+    TestBase_instantiation - line 21
+    TestBase_to_json_string - line 112
+    TestBase_save_to_file - line 158
+    TestBase_from_json_string - line 236
+    TestBase_create - line 290
+    TestBase_load_from_file - line 342
+    TestBase_save_to_file_csv - line 408
+    TestBase_load_from_file_csv - line 486
 """
 import os
 import unittest
