d67cbc7e778f84e07d70f62c66af43ce855e4702
JohnKamaujk <johnjkkamau@gmail.com>
2023-09-11 14:19:44 +0300

    :sparkles:chore:factorial js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+if (isNaN(process.argv[2])) {
+  console.log(1);
+} else {
+  const value = parseInt(process.argv[2]);
+  answer = factorial(value);
+  console.log(answer);
+}
