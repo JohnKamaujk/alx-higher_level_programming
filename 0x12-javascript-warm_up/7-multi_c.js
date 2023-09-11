d5d623f9d22da6bdc272f281bac5a70fb432e7eb
JohnKamaujk <johnjkkamau@gmail.com>
2023-09-11 13:42:40 +0300

    :sparkles:chorre:multi js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 if (isNaN(process.argv[2])){
   console.log('Missing number of occurrences');
 } else {
-  const times = parseInt(process.argv[2]);
+  const times = Number(process.argv[2]);
 
   for (let i = 0; i < times; i++){
     console.log('C is fun');
