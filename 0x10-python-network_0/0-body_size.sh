787c479e2bf49effcf58f670dc1bfb13f50be611
JohnKamaujk <johnjkkamau@gmail.com>
2023-09-28 22:24:19 +0300

    :sparkles:chore:body network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a request to the URL
-curl -s "$1" | wc -c
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s $1 | wc -c
