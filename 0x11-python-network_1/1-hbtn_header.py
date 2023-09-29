fce16e6a451e919687a94d7020f42eae536fd2c1
JohnKamaujk <johnjkkamau@gmail.com>
2023-09-29 12:38:36 +0300

    :sparkles:chore:post request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+except Exception as e:
+    print(f"Error: {e}")
