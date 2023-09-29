6509eaf743ee45718c3d119842dfbb5ca351bd87
JohnKamaujk <johnjkkamau@gmail.com>
2023-09-29 12:43:25 +0300

    :sparkles:chore:hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
 
     with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
+        content = response.read()
 
     print("Body response:")
     print(f"\t- type: {type(content)}")
     print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
