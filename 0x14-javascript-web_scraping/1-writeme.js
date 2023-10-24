7db38022d6379fa909d00e2a45544e34f04a8daa
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-24 12:03:57 +0300

    :sparkles:chore:write to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -10,6 +10,6 @@ if (process.argv.length !== 4) {
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, contentToWrite, (err) => {
   console.log(err || 'Written to file succesfully');
 });
