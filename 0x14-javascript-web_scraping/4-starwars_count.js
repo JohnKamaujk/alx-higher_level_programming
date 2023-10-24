7139a04706ed2a6704833e417975fbc24d40459e
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-24 13:19:17 +0300

    :sparkles:chore:starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Check if the url is provided as a command-line argument
+// Check if the URL is provided as a command-line argument
 if (process.argv.length !== 3) {
-  console.error('Usage: ./3-starwars_count.js <url>');
+  console.error('Usage: ./3-starwars_count.js <URL>');
   process.exit(1);
 }
 
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (!error) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+  } else {
     const filmData = JSON.parse(body);
-    const wedgeAntillesFilms = filmData.results.filter(film => {
-      return film.characters.some((character) => character === 'https://swapi-api.alx-tools.com/api/people/18/');
-    });
-    console.log(wedgeAntillesFilms.length);
+    const wedgeAntillesFilms = filmData.results.reduce((count, film) => {
+      const hasWedgeAntilles = film.characters.find((character) => character.endsWith('/18/'));
+      return count + (hasWedgeAntilles ? 1 : 0);
+    }, 0);
+    console.log(wedgeAntillesFilms);
   }
 });
