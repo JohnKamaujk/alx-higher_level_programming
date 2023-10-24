f3a969fa3e60abd82092eb93391ea875feb62c75
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-24 14:08:25 +0300

    :sparkles:chore:starwars characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,43 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./101-starwars_characters.js <Movie-ID>');
-  process.exit(1);
-}
-
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode}`);
-  } else {
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-    const charactersInOrder = [];
-
-    function fetchCharacterName (characterUrl) {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (!charError && charResponse.statusCode === 200) {
-          const characterData = JSON.parse(charBody);
-          charactersInOrder.push(characterData.name);
-
-          if (charactersInOrder.length === characterUrls.length) {
-            // All character names fetched, print them in order
-            charactersInOrder.forEach((name) => {
-              console.log(name);
-            });
-          }
-        }
-      });
-    }
-
-    // Start fetching character names in order
-    characterUrls.forEach((characterUrl) => {
-      fetchCharacterName(characterUrl);
-    });
+// Make a request to get film data
+request(filmUrl, (error, response, filmBody) => {
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(filmBody).characters;
+    // Start printing characters one by one
+    printCharacters(characters, 0);
   }
 });
+
+// Function to print characters from the characters array
+function printCharacters (characters, index) {
+  if (index < characters.length) {
+    const characterUrl = characters[index];
+    // Make a request to get character data
+    request(characterUrl, (error, response, characterBody) => {
+      if (!error && response.statusCode === 200) {
+        const characterData = JSON.parse(characterBody);
+        console.log(characterData.name);
+        // Move on to the next character
+        printCharacters(characters, index + 1);
+      }
+    });
+  }
+}
