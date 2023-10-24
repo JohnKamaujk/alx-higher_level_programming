7ea4177b9b76ffc07f4163e74ec8aea412841272
JohnKamaujk <johnjkkamau@gmail.com>
2023-10-24 13:02:08 +0300

    :sparkles:chore:starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,7 +10,11 @@ if (process.argv.length !== 3) {
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (!error) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+  } else {
     const filmData = JSON.parse(body);
     const wedgeAntillesFilms = filmData.results.filter(film => {
       return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
